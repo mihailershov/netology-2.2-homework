972eefd4c5056e508e7842fe22316550dd691234
Mihail Ershov <mihershov9@gmail.com>
2017-04-30 00:37:57 +0300

    first working version

--- admin.php
+++ admin.php
@@ -1,7 +1,29 @@
 <?php
 
-?>
+// Если был получен POST-запрос с файлом, то проверяем, подходит ли он
+if (isset($_POST['upload'])) {
 
+    // Определяем массив со всеми файлами из папки с тестами
+    $allFiles = glob('tests/*.json');
+
+    // Определяем загружаемый файл
+    $uploadfile = 'tests/' . basename($_FILES['testfile']['name']);
+
+    // Прогоняем файл по if'ам, если не подходит - выкидываем ошибку
+    if (pathinfo($_FILES['testfile']['name'], PATHINFO_EXTENSION) !== 'json') {
+        $result = "<p class='error'>Можно загружать файлы только с расширением json</p>";
+    } else if ($_FILES["testfile"]["size"] > 1024 * 1024 * 1024) {
+        $result = "<p class='error'>Размер файла превышает три мегабайта</p>";
+    } else if (in_array($uploadfile, $allFiles, true)) {
+        $result = "<p class='error'>Файл с таким именем уже существует.</p>";
+    } else if (move_uploaded_file($_FILES['testfile']['tmp_name'], $uploadfile)) {
+        $result = "<p class='success'>Файл корректен и успешно загружен на сервер</p>";
+    } else {
+        $result = "<p class='error'>Произошла ошибка</p>";
+    }
+}
+
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -14,43 +36,69 @@
 </head>
 <body>
 
-<form action="newTest.php" id="load-json" method="POST" enctype="multipart/form-data">
-    <fieldset>
-        <legend>Загрузите свой тест в формате json</legend>
-        <input type="file" name="testfile" id="uploadfile" required>
-        <input type="submit" value="Добавить в базу" id="submit-upload">
-        <input type="hidden" value="">
-    </fieldset>
-</form>
+<!-- Если файл был отправлен, то выводить информацию о файле и уведомление об успешной загрузке/ошибке -->
 
-<div class="all-tests"><a href="list.php">Все тесты >></a></div>
+<?php if (isset($_POST['upload'])): ?>
+    <a href="../"><div>< Назад</div></a>
+    <?php echo $result; ?>
+    <h1>Служебная информация:</h1>
+    <pre>
+        <?php print_r($allFiles); ?>
+        <hr>
+        <?php   print_r($_FILES); ?>
+    </pre>
+<?php endif; ?>
 
-<form action="list.php" id="create-json">
-    <fieldset>
-        <legend>Или создайте тест прямо на сайте</legend>
-        <div class="question-block1 question-container">
-            <p class="question-number">1 вопрос</p>
-            <div class="question">
-                <p>Вопрос: </p><input type="text" name="question1" required><br>
-            </div>
-            <p>Ответ 1: </p><input type="text" name="answer1" class="answer" required><br>
-            <p>Ответ 2: </p><input type="text" name="answer2" class="answer" required><br>
-            <p>Ответ 3: </p><input type="text" name="answer3" class="answer"><br>
-            <p>Ответ 4: </p><input type="text" name="answer4" class="answer"><br>
-            <p class="correct-answer-p">Правильный ответ:</p>
-            <div class="correct-answer">
-                    1<input type="radio" name="radiobutton" class="first-correct">
-                    2<input type="radio" name="radiobutton" class="second-correct">&nbsp;&nbsp;&nbsp;
-                    3<input type="radio" name="radiobutton" class="third-correct">&nbsp;&nbsp;&nbsp;
+<!-- Если файл или форма теста не была отправлена, то выводить форму загрузки и форму создания теста -->
+
+<?php if (isset($_POST['create']) === false && isset($_POST['upload']) === false): ?>
+
+    <form id="load-json" method="POST" enctype="multipart/form-data">
+        <fieldset>
+            <legend>Загрузите свой тест в формате json</legend>
+            <input type="file" name="testfile" id="uploadfile" required>
+            <input type="submit" value="Добавить в базу" id="submit-upload" name="upload">
+        </fieldset>
+    </form>
+
+    <div class="all-tests">
+        <fieldset>
+            <a href="list.php">Посмотреть все тесты >></a>
+        </fieldset>
+    </div>
+
+    <form method="POST" id="create-json">
+        <fieldset>
+
+            <legend>Или создайте тест прямо на сайте</legend>
+
+            <div class="question-block1 question-container">
+                <p class="question-number">1 вопрос</p>
+                <div class="question">
+                    <p>Вопрос: </p><input type="text" name="question1" required><br>
+                </div>
+                <p>Ответ 1: </p><input type="text" name="answer1" class="answer" required><br>
+                <p>Ответ 2: </p><input type="text" name="answer2" class="answer" required><br>
+                <p>Ответ 3: </p><input type="text" name="answer3" class="answer"><br>
+                <p>Ответ 4: </p><input type="text" name="answer4" class="answer"><br>
+                <p class="correct-answer-p">Правильный ответ:</p>
+                <div class="correct-answer">
+                    1<input type="radio" name="radiobutton" class="first-correct">&nbsp&nbsp;&nbsp;
+                    2<input type="radio" name="radiobutton" class="second-correct">&nbsp&nbsp&nbsp;
+                    3<input type="radio" name="radiobutton" class="third-correct">&nbsp&nbsp;&nbsp;
                     4<input type="radio" name="radiobutton" class="fourth-correct">
+                </div>
             </div>
-        </div>
-        <div class="buttons">
-            <input type="submit" name="create">
-            <button id="add-question" type="button">Добавить еще один вопрос</button>
-        </div>
-    </fieldset>
-</form>
+
+            <div class="buttons">
+                <input type="submit" name="create">
+                <button id="add-question" type="button">Добавить еще один вопрос</button>
+            </div>
+
+        </fieldset>
+    </form>
+
+<?php endif; ?>
 
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 <script src="js/admin.js"></script>
--- list.php
+++ list.php
@@ -1,6 +1,11 @@
 <?php
-    $allFiles = glob('_tests/*.json');
-    $uploadfile = '_tests' . basename($_FILES['testfile']['name']);
+
+    // Определяем массив со всеми файлами из папки с тестами
+    $allFiles = glob('tests/*.json');
+
+    // Определяем загружаемый файл
+    $uploadfile = 'tests' . basename($_FILES['testfile']['name']);
+
 ?>
 <!doctype html>
 <html lang="en">
@@ -9,29 +14,23 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <style>
-        * {
-            font-family: sans-serif;
-        }
-        body {
-            max-width: 1420px;
-            margin: auto;
-        }
-        h1 {
-            display: inline
-        }
-        a {
-            display: inline
-        }
-    </style>
+    <link rel="stylesheet" href="styles/list.css">
 </head>
 <body>
+    <a href="admin.php" class="back"><div>< Назад</div></a>
+    <hr>
+
+    <!-- Цикл, который выводит список всех загруженных файлов -->
+
     <?php foreach ($allFiles as $file): ?>
-        <div>
-            <h1><?php echo str_replace('_tests/', '', $file); ?></h1>
-            <a href="test.php/test?number=<?php echo array_search($file, $allFiles); ?>">Перейти на страницу с тестом</a>
-            <div class="show-content">Показать содержимое файла >>></div>
+
+        <div class="file-block">
+            <h1><?php echo str_replace('tests/', '', $file); ?></h1><br>
+            <em>Загружен: <?php echo date("d-m-Y H:i", filemtime($file)) ?></em><br>
+            <a href="test.php?number=<?php echo array_search($file, $allFiles); ?>">Перейти на страницу с тестом ></a>
         </div>
+        <hr>
+
     <?php endforeach; ?>
 
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
